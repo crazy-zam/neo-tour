cd08e71eedd123804fd1ac6c03c3b17763723de7
Azamat Malikov <crazy-zam@ya.ru>
2024-02-17 16:51:48 +0600

    add onclick yo carousel

--- src/components/UI/Carousel.jsx
+++ src/components/UI/Carousel.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState, Children, cloneElement } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './carousel.module.css';
 const Carousel = ({ offset, discover }) => {
+  const navigate = useNavigate();
   return (
     <div>
       <div className={styles.mainContainer}>
@@ -12,7 +13,18 @@ const Carousel = ({ offset, discover }) => {
             }}
           >
             {discover.map((item) => (
-              <div className={styles.item} key={item.id}>
+              <div
+                className={styles.item}
+                key={item.id}
+                onClick={() => {
+                  navigate(`/trips/${item.id}`);
+                  window.scrollTo({
+                    top: 0,
+                    left: 0,
+                    behavior: 'smooth',
+                  });
+                }}
+              >
                 <img src={item.image} alt="" />
                 <div className={styles.itemTxt}>{item.location}</div>
               </div>
